e36d3769a6f6a5c3b9ce98dcb700cfe6fea3ff67
abhisekChowdhury <a.chowdhury94@gmail.com>
2022-05-29 18:32:48 -0700

    first commit

---
+++ adSkipYoutube.js
@@ -0,0 +1,8 @@
+//Skips youtube ads
+
+setInterval(function() { 
+            var $cross = document.getElementsByClassName("ytp-ad-overlay-close-container")[0]; 
+            var $skip = document.getElementsByClassName("ytp-ad-skip-button")[0]; 
+            if ($cross != undefined) $cross.click(); 
+            if ($skip != undefined) $skip.click() 
+            }, 2000);
