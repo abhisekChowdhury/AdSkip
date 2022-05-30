55ee7d07688f36b04a994ad6ea49c7ec6de0a2ea
abhisekChowdhury <a.chowdhury94@gmail.com>
2022-05-29 18:51:36 -0700

    code modified

--- adSkipYoutube.js
+++ adSkipYoutube.js
@@ -1,8 +1,8 @@
 //Skips youtube ads
 
 setInterval(function() { 
-            var $cross = document.getElementsByClassName("ytp-ad-overlay-close-container")[0]; 
-            var $skip = document.getElementsByClassName("ytp-ad-skip-button")[0]; 
-            if ($cross != undefined) $cross.click(); 
-            if ($skip != undefined) $skip.click() 
+            var $container = document.getElementsByClassName("ytp-ad-overlay-close-container")[0]; 
+            var $skipButton = document.getElementsByClassName("ytp-ad-skip-button")[0]; 
+            if ($container != undefined) $container.click(); 
+            if ($skipButton != undefined) $skipButton.click() 
             }, 2000);
